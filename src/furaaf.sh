e737448f8914abd7f56a30fcc6eb23b8caf4784e
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2023-08-05 08:12:27 -0300

    change: log dir

--- src/furaaf.sh
+++ src/furaaf.sh
@@ -6,13 +6,13 @@
 gcc utils/combinationNoOrder.c -o combinationNoOrder
 all_configurations=$(./combinationNoOrder 4 | tee)
 for config in $all_configurations; do
-    rm result_[$config].txt
+    rm results/tcc/result_[$config].txt
     make
     clear
     for func in $(seq 1 15); do
         ./coleta-info.sh -n 20 -c $config -f $func | tee output-coleta-info.dat
         result=$(cat output-coleta-info.dat | tail -n 6)
-        echo -e $result >> result_[$config].txt
+        echo -e $result >> results/tcc/result_[$config].txt
         rm output-coleta-info.dat
         tput reset
     done;
