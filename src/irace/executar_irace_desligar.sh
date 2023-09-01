f4ad45d796c60af82a2f32b2ed8a8b4f0b133bd0
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2023-09-01 14:38:20 -0300

    add: auto irace

--- src/irace/executar_irace_desligar.sh
+++ src/irace/executar_irace_desligar.sh
@@ -1,5 +1,10 @@
-irace > result-irace-25.txt
+seq="1 2 3 4 5 6 7 8 9 10"
+for i in $seq; do
+    rm -f target-runner
+    cat target-runner-model | sed "s/<ISLAND_NUMBER>/$i/g" > target-runner
+    chmod +x target-runner
+    irace > result-irace-$i.txt
+done;
 
 wait
-
-shutdown -h now
+# shutdown -h now
