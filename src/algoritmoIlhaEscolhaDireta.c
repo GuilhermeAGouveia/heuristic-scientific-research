f75b333b32d01a314bac8b0fc2591a1589108161
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2023-09-09 12:38:42 -0300

    feat: show migration type

--- src/algoritmoIlhaEscolhaDireta.c
+++ src/algoritmoIlhaEscolhaDireta.c
@@ -1,24 +1,24 @@
 /**
  * @brief Este algoritmo de ilha aceita que seja passado qual o conjunto de algoritmos que irá compor o conjunto de ilhas. Os parâmetros são:
- * -A: conjunto de algoritmos que irão compor o conjunto de ilhas. 
+ * -A: conjunto de algoritmos que irão compor o conjunto de ilhas.
  * Exemplo: -A 1,2,3,4,2:
  * Cada valor representa a quantidade de ilhas para cada algoritmo
- * Ordem: 
+ * Ordem:
  * 0 - PSO
  * 1 - DE
  * 2 - ACO
  * 3 - CLONALG
  * 4 - GA
- * 
+ *
  * Exemplo de execução: ./evol -A 1,2,3,4,2 -K 2 -k 3
- * 
+ *
  * @author @gustavo1902
  * @author @ViniciusBastoss
  * @author @caioreius
  * @author @GuilhermeAGouveia
  * Orientador: Prof. Dr. @iagoac
  * @date 2023-08-31
-*/
+ */
 
 #include "algorithms/genetic.h"
 #include "algorithms/pso.h"
@@ -74,13 +74,12 @@ int main(int argc, char *argv[])
     individuo *gbest_individuo = generate_population(1, 10, parameters.domain_function, 15);
     individuo *pbest_individuo = generate_population(1, 10, parameters.domain_function, 15);
     gbest_individuo->fitness = INFINITY;
-    
+
     populacao **populations = calloc(parameters.num_algorithms, sizeof(populacao *));
     int *alg_set = convert_parameter_to_array(parameters.algorithms);
     alg_set = get_algorithms(alg_set, parameters.num_algorithms);
     printf("Algorithms: ");
     printVector(alg_set, parameters.num_algorithms);
-
 
     for (int epoca = 0; epoca < parameters.num_epocas; epoca++)
     {
@@ -113,20 +112,26 @@ int main(int argc, char *argv[])
                 exit(1);
             }
 
-            copy_individuo(get_best_of_population(*populations[alg_pos]),pbest_individuo, parameters.dimension);
+            copy_individuo(get_best_of_population(*populations[alg_pos]), pbest_individuo, parameters.dimension);
             print_individuo(*pbest_individuo, parameters.dimension, alg_pos);
             if (pbest_individuo->fitness < gbest_individuo->fitness)
             {
-                copy_individuo(pbest_individuo,gbest_individuo, parameters.dimension);
+                copy_individuo(pbest_individuo, gbest_individuo, parameters.dimension);
             }
         }
         set_neighbours(populations, parameters.num_algorithms);
         // print_neighbours(populations, parameters.num_algorithms);
-        printf("Migrating...\n");
+
         if (((float)rand() / RAND_MAX) < parameters.choice_random_migrate)
+        {
+            printf("Migrating [RANDOM]...\n");
             random_random_migrate(populations, parameters.num_algorithms, parameters.num_migrations, parameters.dimension, parameters.domain_function, parameters.function_number);
+        }
         else
+        {
+            printf("Migrating [BEST]...\n");
             migrate(populations, parameters.num_algorithms, parameters.num_migrations, parameters.dimension, parameters.domain_function, parameters.function_number);
+        }
     }
 
     printf("Best %lf\n", gbest_individuo->fitness);
