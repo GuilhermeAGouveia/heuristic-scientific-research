055243c9854ee08907c23e72e12d438b8984b13e
ViniciusBastoss <viniciuszt@gmail.com>
2024-11-22 22:03:32 -0300

    feat: allowing the execution of multiple functions in parallel in a furaaf_main.sh instance

--- src/furaaf.sh
+++ src/furaaf.sh
@@ -3,36 +3,10 @@
 # Como executar o script:
 # No makefile, execute o comando "make furaaf"
 
-extract_param() {
-    local params=$1
-    local param=$2
-    echo $params | cut -d"$param" -f 2 | cut -d' ' -f 2
-}
-
-move_arquivos() {
-    origem="$1"
-    destino="$2"
-    nova_pasta="$3"
-
-    mkdir -p "$destino/$nova_pasta" 2>/dev/null
-    mv "$origem"/* "$destino/$nova_pasta"
-}
-
-contar_instancias() {
-    pgrep -f "furaaf.sh" | grep -v "^$$\$" | wc -l
-}
-
-# last_three_digits=$(echo $$ | sed 's/.*\(...\)$/\1/')
-# sleep_time=$(echo "scale=1; ($last_three_digits / 1.8)" | bc)
-
-# # Coloca o script para dormir para que as diferentes instâncias não acessem params.txt ao mesmo tempo
-# sleep $sleep_time
-# echo "Processo $$ dormiu por $sleep_time segundos."
-
 # Caminho do arquivo de lock
 lock_file="paramsLock"
 cores=48
-#cores_per_node=$(echo "$cores / $nodes" | bc)
+
 # Tenta obter o lock para acessar params.txt
 while ! mkdir "$lock_file" 2>/dev/null; do
     echo "Processo $$ aguardando pelo lock."
@@ -59,5 +33,6 @@ rm -rf $lock_file
 
 wait
 
-# Executa o script principal
+echo  "Script END $(date +"%Y-%m-%d %H:%M:%S.%3N")"
+
 
--- src/furaaf_main.sh
+++ src/furaaf_main.sh
@@ -40,35 +40,30 @@ return_free_cores() {
     echo $free_cores  # Use echo para retornar o valor
 }
 
-n_execucoes=2
-first_function=1
-last_function=4
-config=$1
-total_process_file=$2
-cores=$3
-current_n_cores=1
+free_run_function() {
+    while ! mkdir "$total_process_file" 2>/dev/null; do
+    sleep 0.2
+    done
 
-new_config=$(echo $config | sed "s/_/ /g")
-parcial_name=$(extract_param "$new_config" "A")
-#rm -rf logs_genetica/metrics/$config
-#rm -rf logs_genetica/coleta_info/$config
-path_metrics="logs_genetica/metrics/$config"
+    current_total_process=$(head -n 1 "$total_process_file.txt")
+    current_total_process=$(echo "$current_total_process - $1" | bc)
+    sed -i '1d' $total_process_file.txt
+    echo $current_total_process >> $total_process_file.txt
 
-for func in $(seq $first_function $last_function); do
+    rm -r $total_process_file
+
+
+}
+
+run_function() {
+    func=$1
     echo ""
     echo "$config funcao $func"
-    #rm results/tcc/result_[$parcial_name][f$func].txt
-    #current_n_cores=$(( $(return_free_cores) + current_n_cores ))
-    #free_cores=$(echo "$(return_free_cores)" | grep -oE '^-?[0-9]+(\.[0-9]+)?$')
-    free_cores=$(echo "$(return_free_cores)")
-    current_n_cores=$(echo "$free_cores + $current_n_cores" | bc)
-
-    echo "Cores $current_n_cores $config"
 
     temporary_folder=$(date +%H%M%S_%3N)$config
 
     # result=$(./coleta-info.sh -n $n_execucoes -c "$new_config" -f $func -t 10 -Z $temporary_folder | tee output-coleta-info[$parcial_name][f$func].dat)
-    resultado_coleta=$(./coleta-info.sh -n $n_execucoes -c "$new_config" -f $func -t 10 -Z $temporary_folder -C $current_n_cores)
+    resultado_coleta=$(./coleta-info.sh -n $n_execucoes -c "$new_config" -f $func -t 10 -Z $temporary_folder -C $2)
     arquivo_saida="output-coleta-info[$parcial_name][f$func].dat"
     echo "$resultado_coleta" > $arquivo_saida
 
@@ -80,24 +75,73 @@ for func in $(seq $first_function $last_function); do
 
     ./metrics_all $path_metrics $func $n_execucoes &
     wait
+    cores_to_remove=$(($2))
+
+    free_run_function $cores_to_remove
+}
+
+n_execucoes=30
+first_function=1
+last_function=15
+config=$1
+
+total_process_file=$2
+cores=$3
+current_function=$first_function
+metrics_n_cores=0
+function_n_cores=1
+
+new_config=$(echo $config | sed "s/_/ /g")
+parcial_name=$(extract_param "$new_config" "A")
+#rm -rf logs_genetica/metrics/$config
+#rm -rf logs_genetica/coleta_info/$config
+path_metrics="logs_genetica/metrics/$config"
+
+while [ $current_function -le $last_function ]; do
+
+
+     while [  $function_n_cores -eq 0 ]; do
+         function_n_cores=$(echo "$(return_free_cores)")
+     done
+
+
+    if [ $(echo "$function_n_cores + $current_function - 1" | bc) -gt $last_function ]; then
+        free_cores_to_function=$(( $last_function - $current_function + 1 ))
+        metrics_n_cores=$(echo "($function_n_cores - $free_cores_to_function)" | bc)
+        function_n_cores=$free_cores_to_function
+    fi
+
+    metrics_for_all=$(echo "scale=0; ($metrics_n_cores / $function_n_cores) + 1" | bc)
+    metrics_for_first=$(( ($metrics_n_cores % $function_n_cores) + $metrics_for_all))
+
+
+    echo ""
+    echo "-----------------------------------------------------------------------------"
+    echo $config
+    echo "Valor de function_n_cores: $function_n_cores, Metrics:$metrics_n_cores"
+    echo "For_all: $(($metrics_for_all - 1)), For first: $(($metrics_for_first - 1))"
+    echo ""
+
+    for ((i = 0; i < $function_n_cores; i++));do
+        if [ $i -eq 0 ]; then
+           run_function $current_function $metrics_for_first &
+        else
+           run_function $current_function  $metrics_for_all &
+        fi    
+        current_function=$(($current_function + 1))
+        sleep 0.1
+    done
+
+    wait
+
+    function_n_cores=0
+    metrics_n_cores=0
+    
 done
 
 
-while ! mkdir "$total_process_file" 2>/dev/null; do
-    echo "Processo $config aguardando pelo acesso a $total_process_file."
-    sleep 0.2
-done
-echo "Processo $config obteve acesso em END $(date +"%Y-%m-%d %H:%M:%S.%3N")."
-
-current_total_process=$(head -n 1 "$total_process_file.txt")
-current_total_process=$(echo "$current_total_process - $current_n_cores" | bc)
-sed -i '1d' $total_process_file.txt
-echo $current_total_process >> $total_process_file.txt
-
-rm -r $total_process_file
 rm -rf $path_metrics/exe* 
-
-
+echo $config END  $(date +"%Y-%m-%d %H:%M:%S.%3N")
 
 
 # for ((function = $first_function; function <= $last_function; function++)); do
